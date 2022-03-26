6e2cd6365ec66d23574d31fac2d1f7d21920b643
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-26 16:38:45 +0530

    second push

--- personal.practice/src/test/java/personal/practice/FirstClass.java
+++ personal.practice/src/test/java/personal/practice/FirstClass.java
@@ -6,5 +6,13 @@ public class FirstClass {
 @Test
 public void demo() {
 	System.out.println("Demo");
+	
+	
+}
+public void demo1() {
+	System.out.println("demo2");
+}
+public void demo3() {
+	System.out.println("demo3");
 }
 }
