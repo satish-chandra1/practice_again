2c1e70523f6cb8fa4e7ddac951744ec4626f6273
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-26 16:28:22 +0530

    project push

---
+++ fastloop/src/test/java/fastloop/Practice.java
@@ -0,0 +1,16 @@
+package fastloop;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+@Test
+public void men() {
+		System.out.println("hii");
+	}
+@Test
+public void women() {
+	System.out.println("helo");
+}
+
+}
---
+++ fastloop/src/test/java/fastloop/Vision.java
@@ -0,0 +1,15 @@
+package fastloop;
+
+import org.testng.annotations.Test;
+
+public class Vision {
+
+	@Test
+	public void future() {
+		System.out.println("bright");
+	}
+	@Test
+	public void present() {
+		System.out.println("hardwork");
+	}
+}
