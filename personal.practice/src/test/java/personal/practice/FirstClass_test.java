5db34f0df074422fae60a5b9fb0584ccd38b20ee
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-28 13:08:38 +0530

    push

---
+++ personal.practice/src/test/java/personal/practice/FirstClass_test.java
@@ -0,0 +1,39 @@
+package personal.practice;
+
+import org.testng.annotations.Test;
+
+public class FirstClass_test {
+@Test
+public void demo() {
+	System.out.println("Demo");
+	
+	
+}
+public void demo1() {
+	System.out.println("demo2");
+}
+public void demo3() {
+	System.out.println("demo3");
+
+}
+public void demo4() {
+		System.out.println("demo4");
+	}
+public void demo5() {
+	System.out.println("demo5");
+}
+
+public void demo6() {
+	System.out.println("demo6");
+}
+public void demo7() {
+	System.out.println("demo7");
+}
+public void demo19() {
+	System.out.println("demo19");
+}
+public void demo8() {
+	System.out.println("demo8");
+}
+}
+
