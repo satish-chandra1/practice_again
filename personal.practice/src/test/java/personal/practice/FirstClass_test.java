0d73b2408e7a519ecde72c592acdf96fa2dc8a1b
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-28 13:12:51 +0530

    push

--- personal.practice/src/test/java/personal/practice/FirstClass_test.java
+++ personal.practice/src/test/java/personal/practice/FirstClass_test.java
@@ -6,34 +6,38 @@ public class FirstClass_test {
 @Test
 public void demo() {
 	System.out.println("Demo");
-	
-	
-	
-	
+		
 }
+@Test
 public void demo1() {
 	System.out.println("demo2");
 }
+@Test
 public void demo3() {
 	System.out.println("demo3");
 
 }
+@Test
 public void demo4() {
 		System.out.println("demo4");
 	}
+@Test
 public void demo5() {
 	System.out.println("demo5");
 }
-
+@Test
 public void demo6() {
 	System.out.println("demo6");
 }
+@Test
 public void demo7() {
 	System.out.println("demo7");
 }
+@Test
 public void demo19() {
 	System.out.println("demo19");
 }
+@Test
 public void demo8() {
 	System.out.println("demo8");
 }
